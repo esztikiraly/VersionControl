3c952a13ba8edba2784dc2ed6f08a3e9f7e87666
esztikiraly <kiraly.eszter0913@gmail.com>
2021-10-06 22:03:31 +0200

    excel coordinates

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -85,7 +85,22 @@ namespace ExcelExport
             }
 
         }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
 
+            while (dividend>0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
 
 
 
