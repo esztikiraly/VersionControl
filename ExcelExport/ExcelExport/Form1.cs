4904c99826a4ce433022c91797d1f995d66b3039
esztikiraly <kiraly.eszter0913@gmail.com>
2021-10-06 21:06:35 +0200

    import

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace ExcelExport
 {
     public partial class Form1 : Form
     {
+        List<Flat> Flats;
+        RealEstateEntities context = new RealEstateEntities();
+
+        public void LoadData() 
+        {
+            Flats = context.Flats.ToList();
+        }
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
         }
     }
 }
