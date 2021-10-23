cbf06131eded31c7111c3d7c4017e5b71258e963
esztikiraly <kiraly.eszter0913@gmail.com>
2021-10-23 23:27:34 +0200

    dynamic functions

--- webszolg/webszolg/Form1.cs
+++ webszolg/webszolg/Form1.cs
@@ -20,18 +20,25 @@ namespace webszolg
         public Form1()
         {
             InitializeComponent();
+            RefreshData();
+        }
+
+        private void RefreshData()
+        {
+            Rates.Clear();
             webservice();
             dataGridView1.DataSource = Rates;
             chartRateData.DataSource = Rates;
         }
+
         private void webservice()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker1.Value.ToString()
             };
             var response = mnbService.GetExchangeRates(request);
            var result = response.GetExchangeRatesResult;
@@ -71,6 +78,21 @@ namespace webszolg
                 chartArea.AxisY.MajorGrid.Enabled = false;
                 chartArea.AxisY.IsStartedFromZero = false;
             }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
        
