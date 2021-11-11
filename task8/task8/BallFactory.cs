edfc762e3e44c21bbca0fc09bc7f41036492a9df
esztikiraly <kiraly.eszter0913@gmail.com>
2021-11-11 21:20:08 +0100

    interface

--- task8/task8/BallFactory.cs
+++ task8/task8/BallFactory.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using task8.Abstractions;
 
 namespace task8.Entities
 {
-    class BallFactory
+    class BallFactory : IToyFactory
     {
-        public Ball CreateNew()
+        public Toy CreateNew()
         {
             return new Ball();
         }
