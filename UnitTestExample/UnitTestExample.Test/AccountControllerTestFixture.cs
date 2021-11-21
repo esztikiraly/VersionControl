ce86ee1aa42fcc255b0c1c3b7fc7b27d5ae73520
esztikiraly <kiraly.eszter0913@gmail.com>
2021-11-21 20:42:15 +0100

    nekem nem megy a validálás:(

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -10,12 +10,24 @@ namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
     {
-        [Test]
+        [
+    Test,
+    TestCase("abcd1234", false),
+    TestCase("irf@uni-corvinus", false),
+    TestCase("irf.uni-corvinus.hu", false),
+    TestCase("irf@uni-corvinus.hu", true)
+]
         public void TestValidateEmail(string email, bool expectedResult)
         {
+            // Arrange
             var accountController = new AccountController();
+
+            // Act
             var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
             Assert.AreEqual(expectedResult, actualResult);
+
         }
     }
 }
